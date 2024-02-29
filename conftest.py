0fbb72bb330fb447fae793a9cd2265bf7801bd12
Sergei_reshetniak <Twelfth641@gmail.com>
2024-02-29 10:08:03 +0300

    add few more test for personal_page

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
 import pytest
+#TODO: # Use pickle for setup cookies
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 
@@ -10,7 +13,8 @@ def driver(request):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=options)
+    service = Service(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=options, service=service)
     request.cls.driver = driver
     yield driver
     driver.quit()
--- pages/personal_page.py
+++ pages/personal_page.py
@@ -10,24 +10,51 @@ class PersonalPage(BasePage):
 
     PAGE_URL = Links.PERSONAL_PAGE
 
-    FIRST_NAME_FIELD = ("xpath", "//input[@name='firstName']")
-    SAVE_BUTTON = ("xpath", "(//button[@type='submit'])[1]")
-    SPINNER = ("xpath", "//div[@class='oxd-loading-spinner']")
+    FIRST_NAME_FIELD_LOCATOR = ("xpath", "//input[@name='firstName']")
+    MIDDLE_NAME_FIELD_LOCATOR = ("xpath", "//input[@name='middleName']")
+    LAST_NAME_FIELD_LOCATOR = ("xpath", "//input[@name='lastName']")
+    EMPLOYEE_ID_FIELD_LOCATOR = ("xpath", "(//input[contains(@class, 'oxd-input')])[5]")
+    SAVE_BUTTON_LOCATOR = ("xpath", "(//button[@type='submit'])[1]")
+    SPINNER_LOCATOR = ("xpath", "//div[@class='oxd-loading-spinner']")
 
-    def change_name(self, new_name):
-        with allure.step(f"Change name on '{new_name}'"):
-            first_name_field = self.wait.until(EC.element_to_be_clickable(self.FIRST_NAME_FIELD))
+    def change_name(self, new_value):
+        with allure.step(f"Change name on '{new_value}'"):
+            first_name_field = self.wait.until(EC.element_to_be_clickable(self.FIRST_NAME_FIELD_LOCATOR))
             first_name_field.send_keys(Keys.CONTROL + "A")
             first_name_field.send_keys(Keys.BACKSPACE)
-            first_name_field.send_keys(new_name)
-            self.new_name = new_name
+            first_name_field.send_keys(new_value)
+            self.new_value = new_value
+
+    def change_middle_name(self, new_name):
+        with allure.step(f"Change middle name on '{new_name}'"):
+            middle_name_field = self.wait.until(EC.element_to_be_clickable(self.MIDDLE_NAME_FIELD_LOCATOR))
+            middle_name_field.send_keys(Keys.CONTROL + "A")
+            middle_name_field.send_keys(Keys.BACKSPACE)
+            middle_name_field.send_keys(new_name)
+            self.new_value = new_name
+
+    def change_last_name(self, new_value):
+        with allure.step(f"Change last name on '{new_value}'"):
+            middle_name_field = self.wait.until(EC.element_to_be_clickable(self.LAST_NAME_FIELD_LOCATOR))
+            middle_name_field.send_keys(Keys.CONTROL + "A")
+            middle_name_field.send_keys(Keys.BACKSPACE)
+            middle_name_field.send_keys(new_value)
+            self.new_value = new_value
+
+    def change_employee_id(self, new_value):
+        with allure.step(f"Change employee id on '{new_value}'"):
+            middle_name_field = self.wait.until(EC.element_to_be_clickable(self.LAST_NAME_FIELD_LOCATOR))
+            middle_name_field.send_keys(Keys.CONTROL + "A")
+            middle_name_field.send_keys(Keys.BACKSPACE)
+            middle_name_field.send_keys(new_value)
+            self.new_value = new_value
 
     @allure.step("Save changes")
     def save_changes(self):
-        self.wait.until(EC.element_to_be_clickable(self.SAVE_BUTTON)).click()
+        self.wait.until(EC.element_to_be_clickable(self.SAVE_BUTTON_LOCATOR)).click()
 
     @allure.step("Changes has been saved successfully")
-    def is_changes_saved(self):
-        self.wait.until(EC.invisibility_of_element_located(self.SPINNER))
-        self.wait.until(EC.visibility_of_element_located(self.FIRST_NAME_FIELD))
-        self.wait.until(EC.text_to_be_present_in_element_value(self.FIRST_NAME_FIELD, self.new_name))
+    def is_changes_saved(self, field_locator):
+        self.wait.until(EC.invisibility_of_element_located(self.SPINNER_LOCATOR))
+        self.wait.until(EC.visibility_of_element_located(field_locator))
+        self.wait.until(EC.text_to_be_present_in_element_value(field_locator, self.new_value))
